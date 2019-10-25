e9e483d264816aaa217431ce5b5fba4f8aadcf3a
Hasibul Chowdhury <hasibulc91@gmail.com>
2019-10-25 22:54:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array_of_names)
+  array_of_names.each_with_index { |item, index|
+    puts "#{index + 1} #{item}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  
 end
 
 def long_planeteer_calls# code an argument here
